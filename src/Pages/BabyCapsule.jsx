6b3222de8767fd812c2f2c8218fff294c33f9866
Raisul-24 <islamraisul2001@gmail.com>
2025-08-19 21:59:38 +0600

    added baby capsule

---
+++ src/Pages/BabyCapsule.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BabyCapsule = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center my-8">Baby Capsule Service</h1>
+        </div>
+    );
+};
+
+export default BabyCapsule;
